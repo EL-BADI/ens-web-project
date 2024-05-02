9a3431511d37f44132431c7d5aff795bcea3aebe
“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” <badizenikhri92@gmail.com>
2024-05-02 18:26:05 +0100

    handle: comments

--- components/Comments/Comments.tsx
+++ components/Comments/Comments.tsx
@@ -15,7 +15,7 @@ const fetcher = async (url: string) => {
   const res = await fetch(url);
 
   const data = await res.json();
-
+  //
   if (!res.ok) {
     const error = new Error(data.message);
     throw error;
