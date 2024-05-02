ee37775999c3e0215d6dfb6b14f955486e6daa59
“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” <badizenikhri92@gmail.com>
2024-05-02 18:23:05 +0100

    handle: comments

---
+++ app/api/comments/route.ts
@@ -0,0 +1,76 @@
+import { auth } from "@/auth";
+import { db } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (req: NextRequest) => {
+  try {
+    const { searchParams } = new URL(req.url);
+    const postSlug = searchParams.get("postSlug") || "";
+
+    const session = await auth();
+    if (!session) {
+      return new NextResponse(
+        JSON.stringify({ message: "Not Authenticated!" }),
+        {
+          status: 401,
+        }
+      );
+    }
+
+    const comments = await db.comment.findMany({
+      where: {
+        postSlug,
+      },
+      include: {
+        user: true,
+      },
+    });
+
+    return new NextResponse(JSON.stringify(comments), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }),
+      { status: 500 }
+    );
+  }
+};
+
+// CREATE A comment
+export const POST = async (req: NextRequest) => {
+  const session = await auth();
+
+  if (!session) {
+    return new NextResponse(JSON.stringify({ message: "Not Authenticated!" }), {
+      status: 401,
+    });
+  }
+
+  try {
+    const { desc, postSlug } = await req.json();
+
+    if (!desc || !postSlug)
+      return new NextResponse(
+        JSON.stringify({ message: "params not provided" }),
+        {
+          status: 400,
+        }
+      );
+
+    await db.comment.create({
+      data: {
+        postSlug,
+        desc,
+        userEmail: session.user?.email as string,
+      },
+    });
+
+    return new NextResponse("success", { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }),
+      { status: 500 }
+    );
+  }
+};
--- components/Comments/Comments.tsx
+++ components/Comments/Comments.tsx
@@ -6,6 +6,10 @@ import Image from "next/image";
 // import { useSession } from "next-auth/react";
 import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
+import { useSession } from "next-auth/react";
+import axios from "axios";
+import { Comment, User } from "@prisma/client";
+import { cn, formatDate } from "@/lib/utils";
 
 const fetcher = async (url: string) => {
   const res = await fetch(url);
@@ -21,19 +25,18 @@ const fetcher = async (url: string) => {
 };
 
 const Comments = ({ postSlug }: { postSlug: string }) => {
-  //   const { status } = useSession();
-  const status = "authenticated";
+  const { status } = useSession();
   const { data, isPending, refetch } = useQuery({
     queryKey: ["comments"],
-    queryFn: () => fetcher("/lsdjf"),
+    queryFn: () => fetcher(`/api/comments?postSlug=${postSlug}`),
   });
 
   const [desc, setDesc] = useState("");
 
   const handleSubmit = async () => {
-    await fetch("/api/comments", {
-      method: "POST",
-      body: JSON.stringify({ desc, postSlug }),
+    await axios.post("/api/comments", {
+      desc,
+      postSlug,
     });
     refetch();
   };
@@ -58,8 +61,8 @@ const Comments = ({ postSlug }: { postSlug: string }) => {
       <div className={styles.comments}>
         {isPending
           ? "loading.."
-          : data?.map((item) => (
-              <div className={styles.comment} key={item._id}>
+          : data?.map((item: Comment & { user: User }) => (
+              <div className={styles.comment} key={item.id}>
                 <div className={styles.user}>
                   {item?.user?.image && (
                     <Image
@@ -71,8 +74,22 @@ const Comments = ({ postSlug }: { postSlug: string }) => {
                     />
                   )}
                   <div className={styles.userInfo}>
-                    <span className={styles.username}>{item.user.name}</span>
-                    <span className={styles.date}>{item.createdAt}</span>
+                    <span className={styles.username + ""}>
+                      {item.user.name}
+                      <span
+                        className={cn(
+                          "text-xs ml-2",
+                          item.user.isProf
+                            ? " text-rose-500"
+                            : " text-indigo-500"
+                        )}
+                      >
+                        {item.user.isProf ? "professor" : "student"}
+                      </span>
+                    </span>
+                    <span className={styles.date}>
+                      {formatDate(item.createdAt)}
+                    </span>
                   </div>
                 </div>
                 <p className={styles.desc}>{item.desc}</p>
