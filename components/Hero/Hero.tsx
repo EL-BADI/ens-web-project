f5164ac2202fd9c98be9957c8cb9b9461878ede5
EL-BADI <badizenikhri92@gmail.com>
2024-05-16 22:37:18 +0100

    add: toast and disable states to publishers

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -8,8 +8,8 @@ const LottieNoSSR = dynamic(() => import("lottie-react"), {
 
 const Hero = () => {
   return (
-    <div className="relative h-screen flex flex-col items-center justify-center">
-      <div className="absolute -z-10 top-0 left-1/2 -translate-x-2/4 h-full md:w-2/3 w-full bg-gradient-to-r from-indigo-500 to-purple-500 transform scale-[0.35] bg-red-500 rounded-full md:blur-[700px] blur-[400px]" />
+    <div className="relative h-screen flex flex-col items-center justify-center px-1">
+      <div className="absolute -z-10 top-0 left-1/2 -translate-x-2/4 h-full md:w-2/3 w-full bg-gradient-to-r from-indigo-500 to-purple-500 transform scale-[0.4] bg-red-500 rounded-full md:blur-[700px] blur-[300px]" />
       <div className="mb-5 text-center flex flex-col items-center gap-5">
         <h1 className=" heroTitle md:text-5xl text-3xl font-extrabold">
           Ens-Kouba Ai Professors Corner
